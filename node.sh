3de923aaab94117c9da1efe63dc16b03c1ef149e
Minja <2241448+theminjus@users.noreply.github.com>
2020-02-08 01:55:48 +0100

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "------------Installing Git--------------" 
+sudo dnf install git-all -y
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+source ~/.bashrc
+
+echo "-----Setup node versions-------" 
+nvm install 8.9.4
+nvm install 0.12.15
+nvm install 10.19.0
+nvm install 12.15.0
+
+echo "------------setup node-gyp--------------" 
+nvm use 8.9.4
+
+cd ~/
+cat > ~/binding.gyp << 'EOF'
+{
+  "targets": [
+    {
+      "target_name": "binding",
+      "sources": [ "src/binding.cc" ]
+    }
+  ]
+}
+EOF
+
+npm install -g node-gyp
+node-gyp configure
+
